3b5336a946c4cc46cdc6c33148abb3e71d19ba68
Jungmin Lee <56882147+whljm1003@users.noreply.github.com>
2022-07-26 13:54:35 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.29 ms, Memory: 29.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -0,0 +1,17 @@
+function solution(new_id) {
+    let answer = new_id
+    .toLowerCase() // 1
+    .replace(/[^\w-_.]/gm, "") // 2
+    .replace(/\.{2,}/gm, ".") // 3
+    .replace(/(^\.|\.$)/gm, "") // 4
+    .replace(/^$/, 'a') // 5
+    .slice(0, 15).replace(/\.$/, ''); // 6
+    
+    while(answer.length < 3) {
+        answer += answer[answer.length-1];    
+    } // 7
+    
+    return answer;
+    
+    
+}
