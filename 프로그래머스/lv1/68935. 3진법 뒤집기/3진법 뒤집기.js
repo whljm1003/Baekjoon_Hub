6ae8155b2e48b6a5f4b8d0348be3b8258341897c
Jungmin Lee <56882147+whljm1003@users.noreply.github.com>
2022-07-26 13:56:22 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 30.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -0,0 +1,7 @@
+// 10진수를 다른 진수로 변환하기 위해서는 toString()을,
+// 다른 진수를 10진수로 변환하기 위해서는 parseInt()를 쓴다.
+
+function solution(n) {
+    let ternary = [...n.toString(3)].reverse().join(""); 
+    return parseInt(ternary,3);
+}
--- 프로그래머스/lv1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/README.md
@@ -1,6 +1,6 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/68935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
 
 ### 성능 요약
 
