b6ae5db6927b2ddbeee12c5142eaeec4ea04a28a
Jungmin Lee <56882147+whljm1003@users.noreply.github.com>
2022-07-29 14:58:18 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.18 ms, Memory: 30.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,6 +1,4 @@
 function solution(s) {
-    let answer = "";
     s = s.split(" ").sort((a,b) => a-b);
-    answer = `${s[0]} ${s[s.length-1]}`;
-    return answer;
+    return `${Math.min(...s)} ${Math.max(...s)}`    
 }
