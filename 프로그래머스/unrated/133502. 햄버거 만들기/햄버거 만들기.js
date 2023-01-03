85333d244943a7461b60f997d01e699be8d7e39c
Jungmin Lee <56882147+whljm1003@users.noreply.github.com>
2023-01-04 00:31:35 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.js
@@ -1,23 +1,13 @@
 function solution(ingredient) {
-    var answer = 0;
+    let count = 0;
 
-    let i = 0;
-    let latest = [0];
-
-    while(i < ingredient.length-3) {
-        if(ingredient[i] == 1 && ingredient[i+1] == 2 &&
-           ingredient[i+2] == 3 && ingredient[i+3] == 1) {
-            answer++
-            ingredient.splice(i, 4)
-
-            i = latest.pop()
+    for (let i = 0; i < ingredient.length; i++) {
+        if (ingredient.slice(i, i + 4).join('') === '1231') {
+            count++;
+            ingredient.splice(i, 4);
+            i -= 3;
         }
-        else if(ingredient[i] == 1) {
-            latest.push(i)
-            i++
-        }
-        else i++
     }
 
-    return answer;
+    return count;
 }
