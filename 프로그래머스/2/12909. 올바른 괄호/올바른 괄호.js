56460b751891ff0a433322abc5777c60a9c1dc68
Jungmin Lee <56882147+whljm1003@users.noreply.github.com>
2025-04-12 11:53:24 +0900

    [level 2] Title: 올바른 괄호, Time: 29.17 ms, Memory: 37.1 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,18 +1,14 @@
 // 괄호 (스택 문제)
 function solution(s){
   const stack = []
-  const arr = s.split('');
 
-  for(let i = 0 ; i< arr.length ; i++) {
-    const target = arr[i];
-    if(target === ")") {
-      if(stack.length === 0) {
-        return false;
-      }
-      stack.pop();
-    }
+  for(let i = 0 ; i< s.length ; i++) {
+    const target = s[i];
     
-    if(target === "(") {
+    if(target === ")") {
+      if(stack.length === 0) return false;
+      stack.pop();
+    } else if(target === "(") {
       stack.push(target);
     }
 
