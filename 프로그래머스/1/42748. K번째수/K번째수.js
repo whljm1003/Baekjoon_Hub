8e0508afaaa7668bfe02e6a62865a00dd76915a7
Jungmin Lee <56882147+whljm1003@users.noreply.github.com>
2025-04-18 20:29:01 +0900

    [level 1] Title: K번째수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,14 +1,15 @@
 function solution(array, commands) {
     var answer = [];
-
-    for(let i = 0 ; i < commands.length ; i++) {
-        const start = commands[i][0] - 1;
+    
+    
+    for(let i = 0 ; i < commands.length ; i ++) {
+        const start = commands[i][0] - 1; // 2
         const end = commands[i][1];
-        const target = commands[i][2] - 1;
-        
-        answer.push(array.slice(start,end).sort((a,b) => a - b)[target])
-    }
+        const target = commands[i][2];
 
+        
+        answer.push(array.slice(start,end).sort((a,b) => a-b)[target - 1]);
+    }
+    
     return answer;
 }
-
