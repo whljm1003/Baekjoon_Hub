643b34d0b26be1fb487fc81ac5b77a84538af31d
Jungmin Lee <56882147+whljm1003@users.noreply.github.com>
2025-04-20 02:51:05 +0900

    [level 1] Title: 내적, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,9 +1,5 @@
 function solution(a, b) {
-    let sum = 0;
-
-    for(let i = 0 ; i < a.length ; i++) {
-        sum += a[i] * b[i];
-    }
-
-    return sum;
+    const answer = a.reduce((acc,cur,i) => acc + (cur * b[i]), 0);
+    
+    return answer;
 }
