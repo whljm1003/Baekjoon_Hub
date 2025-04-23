f42fecb441ad8a200ac6e998e8c3507e5987a958
Jungmin Lee <56882147+whljm1003@users.noreply.github.com>
2025-04-23 14:55:24 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.25 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,31 +1,31 @@
 const alphabet = {  
- zero: "0",
- one: "1",
- two: "2",
- three: "3",
- four: "4",
- five: "5",
- six: "6",
- seven: "7",
- eight: "8",
- nine: "9"
-}
+  zero: "0",
+  one: "1",
+  two: "2",
+  three: "3",
+  four: "4",
+  five: "5",
+  six: "6",
+  seven: "7",
+  eight: "8",
+  nine: "9"
+};
 
 function solution(s) {
-  let answer = [];
+  let result = "";
   let word = "";
-  
-  for(const string of s) {
-    !isNaN(Number(string)) ? answer.push(string) :word += string;
-    if(alphabet[word]) {
-      answer.push(word);
-      word = "";
+
+  for (const ch of s) {
+    if (!isNaN(ch)) {
+      result += ch;
+    } else {
+      word += ch;
+      if (alphabet[word]) {
+        result += alphabet[word];
+        word = "";
+      }
     }
-      
   }
-  
-  for(let i = 0 ; i <= answer.length ; i++) {
-    if(alphabet[answer[i]]) answer[i] = alphabet[answer[i]];
-  }
-    return Number(answer.join(''));
+
+  return Number(result);
 }
