0240b91d5fae43702d6eca231fc5f0fdb6c90268
Jungmin Lee <56882147+whljm1003@users.noreply.github.com>
2025-10-21 21:13:36 +0900

    [level 2] Title: 짝지어 제거하기, Time: 99.69 ms, Memory: 50.9 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -1,13 +1,12 @@
-function solution(s) {
+function solution(s){
     const stack = [];
-    
-    for(const char of s) {
-        if(stack.length > 0 && stack.at(-1) === char) {
-            stack.pop()
-        }else{
-        stack.push(char);
-        }
+  
+    for(const alphabet of s) {
+      const target = stack.at(-1);
+      alphabet === target ? stack.pop() : stack.push(alphabet);
     }
+    
+  
 
     return stack.length === 0 ? 1 : 0;
 }
